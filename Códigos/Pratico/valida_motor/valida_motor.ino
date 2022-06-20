c36071397db0ff3622c393d9e73e6a8669fb6025
Matheus Ferreira Costa <matheeusferreeira@hotmail.com>
2022-06-19 22:14:48 -0300

    Update em valida_motor
    Atualização do código que valida o sentido de giro dos motores.

--- Códigos/Pratico/valida_motor/valida_motor.ino
+++ Códigos/Pratico/valida_motor/valida_motor.ino
@@ -1,26 +1,25 @@
-const int IN1 = 7;
-const int IN2 = 8;
-const int IN3 = 10;
-const int IN4 = 11;
-const int EN_MOTOR_A = 6;
-const int EN_MOTOR_B = 9;
+const int IN1 = 5;    // Motor A
+const int IN2 = 6;    // Motor A
+const int IN3 = 9;    // Motor B
+const int IN4 = 11;   // Motor B
 
 void setup() {
-  pinMode(EN_MOTOR_A, OUTPUT);
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
-  pinMode(EN_MOTOR_B, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
-
 }
 
 void loop() {
-  analogWrite(EN_MOTOR_A, 120);
-  digitalWrite(IN1, 1);     // Controlando o Motor da Direita para F.
-  digitalWrite(IN2, 0);      // Controlando o Motor da Direita para T.
-  analogWrite(EN_MOTOR_B, 120);  
-  digitalWrite(IN3, 1);     // Controlando o Motor da Esquerda para F.
-  digitalWrite(IN4, 0);      // Controlando o Motor da Esquerda para T.
+  analogWrite(IN1, 0);
+  analogWrite(IN2, 120);    // Controlando o Motor A para F (Sentido positivo de accX do sensor e negtivo de KalAngleY)
+  analogWrite(IN3, 0);
+  analogWrite(IN4, 120);    // Controlando o Motor B para F
+  delay(10000);
 
+  analogWrite(IN1, 120);    // Controlando o Motor A para T (Sentido negativo de de accX do sensor e positivo de KalAngleY)
+  analogWrite(IN2, 0);
+  analogWrite(IN3, 120);    // Controlando o Motor B para T
+  analogWrite(IN4, 0);
+  delay(10000);
 }
